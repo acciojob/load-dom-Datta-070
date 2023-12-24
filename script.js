fe93bbbb0171bb4ec6762f17b03c6cc126fb5842
Chinmay Datt D <chinmaydatt003@gmail.com>
2023-12-24 12:34:42 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,4 @@
 //your JS code here. If required.
-document.addEventListener('DOMContentLoaded', f(){
-	var body= document.body;
-	bodyElement.innerHTML = '';
-	var ans=document.createElement('p');
-	ans.textContent='DOM load success';
-	body.appendChild(ans);
+document.addEventListener("DOMContentLoaded", function() {
+  document.write("DOM load success");
 });
